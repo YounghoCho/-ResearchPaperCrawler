1852cd82dcdb80187cde7c50dc954c4678ea8272
Youngho Jo <dodghek1@naver.com>
2019-07-19 04:13:19 +0900

    functional test finished

--- test.js
+++ test.js
@@ -1,61 +1,74 @@
-//에러 : 첫번째 문서가 자꾸 tag값이 null로 나와서.. continue 넣어놓음.
-console.log("프로그램 시작");
-const keywords = ["인공지능"];
+const keywords = ["인공지능", "머신러닝"];
 
 const puppeteer = require('puppeteer');
 puppeteer.launch().then(async browser => {
     (async () => {
-        const browser = await puppeteer.launch({headless: false}) //open browser
-        // const browser = await puppeteer.launch()
-        console.log("크롤러 시작");
-        const page = await browser.newPage()
-        //visit
-        const navigationPromise = page.waitForNavigation()
-        // await page.goto('http://koreascience.or.kr/main.page?&lang=ko')
-//####### while
-        await page.goto('http://koreascience.or.kr/search.page?keywords=' + keywords[0]);
-        await page.setViewport({width: 1440, height: 716})
-        console.log("방문 완료");
 
-        let key = 4;
-        let count = 4;
-        // while (true) {  //한번에 10개문서를 5회 돈다.
-        while (count < 7) {  //한번에 10개문서를 5회 돈다.
-            console.log("============싸이클===========");
-            key = 4; //현 키워드 결과 첫 검색결과 노드의 child가 4부터 시작하게 되어있음.
-            while (key < 14) { //ISSUE 1 : 원래 key의 범위는 4 < 14 여야하는데 7개까지 다운받고 timedout 3000ms 에러발생 => 11번째부터 pdf버튼의 위치가 바뀌었기 때문
-                const isExist = await page.evaluate((key) => {
-                    const links = Array.from(document.querySelectorAll('.srched-box:nth-child(' + key + ') > .d-lg-flex > .list-unstyled > .list-item > .clear > .btn:nth-child(3)'));
-                    return links.map(link => link.nodeName);
-                }, key); //evaluate함수에 매개변수를 undefined안되게 전달하는 방법
-            console.log("key is : " + key + "\nelement is : " + isExist);
-                //3번째에 pdf 버튼이 없는 경우는 첫번째 버튼 위치에 있다.
-                if (isExist == '') {
-//                    continue;
-                    // await page.waitForSelector('.srched-box:nth-child(' + key + ') > .d-lg-flex > .list-unstyled > .list-item > .clear > .btn');
-                    // await page.click('.srched-box:nth-child(' + key + ') > .d-lg-flex > .list-unstyled > .list-item > .clear > .btn');
-                    key++;
-                } else {
-                    // await page.waitForSelector('.srched-box:nth-child(' + key + ') > .d-lg-flex > .list-unstyled > .list-item > .clear > .btn:nth-child(3)');
-                    // await page.click('.srched-box:nth-child(' + key + ') > .d-lg-flex > .list-unstyled > .list-item > .clear > .btn:nth-child(3)');
-                    key++;
+        let lengthOfKeywords = keywords.length, startPoint = 0;
+        while(startPoint < lengthOfKeywords) {
+
+            const browser = await puppeteer.launch({headless: false}); //open browser
+            // const browser = await puppeteer.launch() // for test
+            log("크롤러 시작");
+            const page = await browser.newPage();
+            const navigationPromise = page.waitForNavigation();
+            await page.goto('http://koreascience.or.kr/search.page?keywords=' + keywords[startPoint]);
+            await page.setViewport({width: 1440, height: 716});
+            startPoint++;
+
+            //set language
+            await page.waitForSelector('.navbar #chooseLanguageDropdownMenuLink');
+            await page.click('.navbar #chooseLanguageDropdownMenuLink');
+            await page.waitForSelector('.container > .navbar-nav > .nav-item > .dropdown-menu > .dropdown-item:nth-child(1)');
+            await page.click('.container > .navbar-nav > .nav-item > .dropdown-menu > .dropdown-item:nth-child(1)');
+            await navigationPromise;
+            log("방문 완료");
+
+            let key = 4, count = 4, i = 1;
+            while (count < 5) {  //N번째 page까지 반복한다(2페이지 버튼의 index is 4)
+                log("============" + i + "차 싸이클===========");
+                i++;
+                key = 4; //초기화 : 검색 결과의 첫 자식노드의 index is 4
+                while (key < 14) { //ERROR 1 : timedout 3000ms => puppeteer는 페이지가 30초이상 응답없으면 에러다. 원인은, 특정위치의 버튼을 누르라고 했는데 버튼이 없었기 때문이다.
+                    await sleep(1000);
+                    //ERROR 2 : evaluate에서 얻은 isExist 값이 일정하지 않다 => 동기화가 안되서? => await로 선언되있잖아 => 그럼뭐지? => 페이지가 안떴는데 evaluate가 동작해서 그래 => sleep으로 해결
+                    const isExist = await page.evaluate((key) => {
+                        const links = Array.from(document.querySelectorAll('.srched-box:nth-child(' + key + ') > .d-lg-flex > .list-unstyled > .list-item > .clear > .btn:nth-child(3)'));
+                        console.log("뭐야대체 : " + links.nodeName);
+                        return links.map(link => link.nodeName);
+                    }, key); //ERROR 3 : 동적 함수에 key 값이 전달되지 않고 undefined로 뜸 => 전달하는 방법은 맨뒤에 key를 싣어보냄(공식 페이지 참고)
+                    log("key is : " + (key - 3) + "\nEle is : " + isExist);
+
+                    //PDF다운 버튼 위치 : 첫번쨰 혹은 세번쨰
+                    if (isExist == '') {
+                        // await page.waitForSelector('.srched-box:nth-child(' + key + ') > .d-lg-flex > .list-unstyled > .list-item > .clear > .btn');
+                        // await page.click('.srched-box:nth-child(' + key + ') > .d-lg-flex > .list-unstyled > .list-item > .clear > .btn');
+                        key++;
+                    } else {
+                        // await page.waitForSelector('.srched-box:nth-child(' + key + ') > .d-lg-flex > .list-unstyled > .list-item > .clear > .btn:nth-child(3)');
+                        // await page.click('.srched-box:nth-child(' + key + ') > .d-lg-flex > .list-unstyled > .list-item > .clear > .btn:nth-child(3)');
+                        key++;
+                    }
                 }
-            }
-            console.log("마지막 key is " + key);
-            //Going to next Page //여기도 2번페이지의 인덱스가 4부터 시작한다
-            console.log("count is : " + count);
-            if (count > 9)
-                count = 4;
-            else {
-                console.log("버튼 누르기");
-                await page.waitForSelector('#search-result > .g-mb-100 > .list-inline > .list-inline-item:nth-child(' + count + ') > .u-pagination-v1__item');
-                await page.click('#search-result > .g-mb-100 > .list-inline > .list-inline-item:nth-child(' + count + ') > .u-pagination-v1__item');
-                count++;
-            }
-        }
-        //   await browser.close()
+
+                //go to next page
+                if (count > 9) count = 4;
+                else {
+                    log("다음 페이지로..");
+                    await page.waitForSelector('#search-result > .g-mb-100 > .list-inline > .list-inline-item:nth-child(' + count + ') > .u-pagination-v1__item');
+                    await page.click('#search-result > .g-mb-100 > .list-inline > .list-inline-item:nth-child(' + count + ') > .u-pagination-v1__item');
+                    count++;
+                } //end LOOP1 : 10 contents
+            } //end LOOP2 : pages
+            await browser.close();
+        } //end LOOP3 : keywords
     })()
 });
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+function sleep(time) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, time);
+    });
+}
+function log(string){
+    console.log(string);
 }
